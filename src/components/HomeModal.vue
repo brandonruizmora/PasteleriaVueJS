ffa1798ca06953e9ed42b9bde774ee3af674813c
brandonruizmora <brandon_ruizmor@outlook.es>
2022-03-24 21:23:46 -0600

    added file deleted

---
+++ src/components/HomeModal.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="container rounded border border-dark my-5">
+        <div class="row">
+            <div class="col-12 my-5">
+                <h2 class="text-center text-pink">Ultimos pasteles creados</h2>
+            </div>
+            <div class="col-12">
+                <div class="card-group">
+                    <div class="card" style="width: 18rem;">
+                        <img
+                            src="https://www.wiltonenespanol.com/wp-content/uploads/2020/03/spring-tip-cake.jpg"
+                            class="card-img-top"
+                            alt="pastel 1"
+                        />
+                        <div class="card-body">
+                            <p class="card-text">
+                                Pastel de colores.
+                                <br />$100.00
+                            </p>
+                            <ul>
+                                <li>Decorado: Betún</li>
+                                <li>Sabor: Vainilla</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="card" style="width: 18rem;">
+                        <img
+                            src="https://saboryestilo.com.mx/wp-content/uploads/elementor/thumbs/Pastel-Chocolate-Fresas-p1dn1sidprk8pdtt6a9qnc9mzikywo18tsy01ymog0.jpg"
+                            class="card-img-top"
+                            alt="pastel 2"
+                        />
+                        <div class="card-body">
+                            <p class="card-text">
+                                Pastel de chocolate con fresas.
+                                <br />$100.00
+                            </p>
+                            <ul>
+                                <li>Decorado: Fresas</li>
+                                <li>Sabor: Chocolate</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="card" style="width: 18rem;">
+                        <img
+                            src="https://thesweetmolcajete.com/wp-content/uploads/2020/06/WPRM-Receta-de-Pastel-de-Tres-Leches.jpg"
+                            class="card-img-top"
+                            alt="pastel 3"
+                        />
+                        <div class="card-body">
+                            <p class="card-text">
+                                Pastel vainilla frutos.
+                                <br />$100.00
+                            </p>
+                            <ul>
+                                <li>Decorado: Frutas</li>
+                                <li>Sabor: Galleta</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
